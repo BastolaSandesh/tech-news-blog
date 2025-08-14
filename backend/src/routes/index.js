f991756807a3702a07f98c4f1c2e320f98bf4008
Sandesh Bastola <humanityproject123@gmail.com>
2025-08-15 01:27:11 +0545

    Remove backend code for Vercel deployment

--- backend/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var rssController_1 = require("../controllers/rssController");
-var router = (0, express_1.Router)();
-router.get("/rss", rssController_1.fetchRSSFeeds);
-exports.default = router;
